48351d76ad156510cd00738c537719dc758d8efe
sarasvatidpm11 <114864461+sarasvatidpm11@users.noreply.github.com>
2022-12-17 20:42:24 +0800

    1st-Assignment
    Return enemy to the starting point and Spawn enemy randomly

---
+++ 1st-Assignment/Enemy.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Enemy(){//constractor
+        setRotation(180);
+    }
+    
+    public Enemy(int speed){//1 class can have many constractor
+        setRotation(180);
+        this.speed = speed;
+    }
+    
+    public int speed = 2;
+    
+    public void act()
+    {
+        move(speed);
+        
+        if(isAtEdge()){
+            getWorld().removeObject(this);
+        }
+    }
+}
