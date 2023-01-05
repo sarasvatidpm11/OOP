acbe8c4ad6b5dd434064858872dbad169a2bfda4
sarasvatidpm11 <114864461+sarasvatidpm11@users.noreply.github.com>
2023-01-05 14:12:06 +0800

    Project Greenfoot Oracle Academy

---
+++ PROJECT GREENFOOT ORACLE ACADEMY/3.6/JF_V02_S03_L06PrjStudent/Robot.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Robot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Robot extends Actor
+{
+    /**
+     * Act - do whatever the Robot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        robotMovement();
+        detectWallCollision();
+        detectBlockCollision();
+    }
+    
+    public void robotMovement()
+    {
+        //The code for the robot movement will add in the next project section
+    }
+    
+    public void detectWallCollision()
+    {
+        if (isTouching(Wall.class)){
+            setLocation(48,50);
+        }
+    }
+    
+    public void detectBlockCollision()
+    {
+        if (isTouching(Block.class)){
+            setLocation(48,50);
+        }
+    }
+}
